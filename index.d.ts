3ecd473362c83e87e99d2ef758e947a20817e99a
Luis Camargo <luiska1993@gmail.com>
2022-08-27 21:21:34 -0400

    chore: update type

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,15 @@
 declare const rtokens: {
-  [x: string]: {
-    address: string;
-    name: string;
-    symbol: string;
-    decimals: number;
-    logo: string;
-    website?: string;
-    repository?: string;
-    about?: string;
+  [chainId: number]: {
+    [tokenAddress: string]: {
+      address: string;
+      name: string;
+      symbol: string;
+      decimals: number;
+      logo: string;
+      website?: string;
+      repository?: string;
+      about?: string;
+    };
   };
 };
 export = rtokens;
