a0ac45651f79ee3bea0a468e05543ff55c4a0deb
Luis Camargo <luiska1993@gmail.com>
2023-10-05 09:38:02 -0600

    chore: update types

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,36 @@
 declare const rtokens: {
-  [tokenAddress: string]: {
-    address: string;
-    name: string;
-    symbol: string;
-    decimals: number;
-    logo?: string;
-    about?: string;
-    website?: string;
-    governance?: {
-      voting?: string;
-      discussion?: string;
-    };
-    support?: {
-      email?: string;
-      url?: string;
-    };
-    social?: {
-      blog?: string;
-      chat?: string;
-      facebook?: string;
-      forum?: string;
-      github?: string;
-      gitter?: string;
-      instagram?: string;
-      linkedin?: string;
-      reddit?: string;
-      slack?: string;
-      telegram?: string;
-      twitter?: string;
-      youtube?: string;
+  [chainId: number]: {
+    [tokenAddress: string]: {
+      address: string;
+      name: string;
+      symbol: string;
+      decimals: number;
+      logo?: string;
+      about?: string;
+      website?: string;
+      governance?: {
+        voting?: string;
+        discussion?: string;
+      };
+      support?: {
+        email?: string;
+        url?: string;
+      };
+      social?: {
+        blog?: string;
+        chat?: string;
+        facebook?: string;
+        forum?: string;
+        github?: string;
+        gitter?: string;
+        instagram?: string;
+        linkedin?: string;
+        reddit?: string;
+        slack?: string;
+        telegram?: string;
+        twitter?: string;
+        youtube?: string;
+      };
     };
   };
 };
